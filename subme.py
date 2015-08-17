ea6d0d08f8d58df913ecb753835cf7f4a4966f76
evuez <helloevuez@gmail.com>
2015-08-17 14:24:38 +0200

    Add temporary functions for search and download

--- subme.py
+++ subme.py
@@ -1,23 +1,42 @@
+from importlib import import_module
+from os import path
 from plugins import logger
 from plugins import NoSubsError
-from importlib import import_module
 
 
 PLUGINS = {'opensubtitles'}
 
+media = 'subme.py'
 
-path = 'test/bkd.avi'
-subtitles = []
-for plugin in PLUGINS:
-	logger.info("Looking for subtitles with %s", plugin)
-	wrapper = import_module('plugins.{}'.format(plugin))
-	try:
-		results = getattr(wrapper, 'search')(path)
-		subtitles.extend(results)
-		logger.info("Found %d subtitles with %s", len(results), plugin)
-	except NoSubsError:
-		logger.error("No subtitles found with %s", plugin)
-	except FileNotFoundError:
-		logger.error("Cannot find file or directory %s", path)
+def check_path(media):
+	if not path.exists(media):
+		logger.error("Cannot find file or directory %s", media)
+		exit(1)
 
-subtitles = sorted(subtitles, key=lambda k: k['rating'])
+
+def search(media):
+	subtitles = []
+	for plugin in PLUGINS:
+		logger.info("Looking for subtitles with %s...", plugin)
+		wrapper = import_module('plugins.{}'.format(plugin))
+		try:
+			results = getattr(wrapper, 'search')(media)
+			subtitles.extend(results)
+			logger.info("Found %d subtitles with %s!", len(results), plugin)
+		except NoSubsError:
+			logger.error("No subtitles found with %s!", plugin)
+
+	if not subtitles:
+		logger.error("No subtitle found...")
+		exit(1)
+	else:
+		logger.info("Found %d subtitles!", len(subtitles))
+		return sorted(subtitles, key=lambda k: k['rating'], reverse=True)
+
+
+def download(media, subs):
+	logger.info("Downloading best subtitle...")
+
+
+check_path(media)
+download(media, search(media))
